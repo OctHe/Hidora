3c872b9365a95bd5886380c4ef9d2a1191bb6e15
Shiyue <hsy1995313@gmail.com>
2026-05-21 00:16:14 +0800

    Use template for config file

---
+++ preinstall.py
@@ -0,0 +1,19 @@
+
+template = "blueprint/custom_fedora_template.toml"
+config_dir = "./config/"
+output_file = "custom_fedora.toml"
+
+with open(config_dir + "tmux.conf", encoding="utf-8") as f:
+    tmux_config = f.read()
+with open(config_dir + "vimrc", encoding="utf-8") as f:
+    vim_config = f.read()
+with open(config_dir + "config.fish", encoding="utf-8") as f:
+    fish_config = f.read()
+with open(template, encoding="utf-8") as f:
+    blueprint = f.read()
+
+blueprint = blueprint.replace("{{INSERT_TMUX_CONFIG}}", tmux_config)
+blueprint = blueprint.replace("{{INSERT_VIM_CONFIG}}", vim_config)
+blueprint = blueprint.replace("{{INSERT_FISH_CONFIG}}", fish_config)
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(blueprint)
